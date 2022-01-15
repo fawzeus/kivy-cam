9bed2f43b024929118f75a6414bbc58b2f103c85
fawzirjili <rjilifawzi@gmail.com>
2022-01-15 20:14:42 +0100

    first commit

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ Builder.load_file("myapplayout.kv")
 
 
 class AndroidCamera(Camera):
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     camera_resolution = (640, 480)
     face_resolution = (128, 96)
     ratio = camera_resolution[0] / face_resolution[0]
@@ -39,22 +38,10 @@ class AndroidCamera(Camera):
     def frame_to_screen(self, frame):
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        self.face_det(frame_rgb)
 
         flipped = np.flip(frame_rgb, 0)
         buf = flipped.tostring()
         self.texture.blit_buffer(buf, colorfmt='rgb', bufferfmt='ubyte')
-
-    def face_det(self, frame):
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        resized = cv2.resize(
-            gray, (self.face_resolution[1], self.face_resolution[0]))
-        faces = self.face_cascade.detectMultiScale(resized, 1.3, 2)
-        if len(faces) != 0:
-            face = faces[np.argmax(faces[:, 3])]
-            x, y, w, h = face
-            cv2.rectangle(frame, (int(x * self.ratio), int(y * self.ratio)),
-                          (int((x + w) * self.ratio), int((y + h) * self.ratio)), (0, 255, 0), 2)
 
 
 class MyLayout(BoxLayout):
